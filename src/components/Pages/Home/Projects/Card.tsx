2ccb07ff081cc12be603930a81aeb77d55bae434
MPCL5 <mpcl5mail@gmail.com>
2023-11-02 14:07:03 +0300

    added serverside fetching for stars count

--- src/components/Pages/Home/Projects/Card.tsx
+++ src/components/Pages/Home/Projects/Card.tsx
@@ -1,6 +1,4 @@
-'use client'
 import styles from './Card.module.scss'
-import {useEffect, useState} from "react";
 import clsx from "clsx";
 import {Button} from "@/components/Common/Button";
 import Link from "next/link";
@@ -16,28 +14,18 @@ interface IGitResponse {
     stargazers_count: number,
 }
 
-export const ProjectCard = ({data}: { data: IProjectCardData }) => {
-    const [starCount, setStarCount] = useState<number | null>(null)
+async function getStarCount(repo_link: string): Promise<number> {
+    const response = await fetch(`https://api.github.com/repos/${repo_link}`)
+    if (!response.ok) {
+        throw new Error("Something went wrong while fetching data from github")
+    }
 
-    // TODO: fix this ana fetch them in serverside.
-    useEffect(() => {
-        // Let's do not comply with clean code rules. I am tired rn.
-        fetch(`https://api.github.com/repos/${data.link}`)
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Something went wrong while fetching data from github')
-                }
+    const data = await response.json() as IGitResponse;
+    return data.stargazers_count;
+}
 
-                return response.json() as Promise<IGitResponse>
-            })
-            .then(data => {
-                setStarCount(data.stargazers_count)
-            })
-            .catch((err: Error) => {
-                console.log(err.message)
-            })
-
-    }, [data.link])
+export const ProjectCard = async ({data}: { data: IProjectCardData }) => {
+    const starCount = await getStarCount(data.link);
 
     return (
         <div className={clsx(styles.main, 'row')}>
@@ -47,7 +35,7 @@ export const ProjectCard = ({data}: { data: IProjectCardData }) => {
                 <div className={'d-flex'}>
                     <div>PyTorch</div>
                     <div>{data.date}</div>
-                    <div>{starCount}</div>
+                    <div>star: {starCount}</div>
                 </div>
             </div>
             <div className={'col-3'}>
