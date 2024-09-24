3824ffb7fa00194b4e8014901ab87c56cf51527c
MPCL5 <mpcl5mail@gmail.com>
2024-09-24 12:05:35 +0300

    minor bug fix

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -10,7 +10,7 @@ import {MobileSlider} from "./MobileSlider";
 import {useState} from "react";
 
 export const Header = () => {
-    const [isNavOpen, setIsNaveOpen] = useState(true)
+    const [isNavOpen, setIsNaveOpen] = useState(false)
 
     return <header className={clsx("container d-flex justify-content-md-between align-items-center", styles.main)}>
         <SandwichButton onClick={() => setIsNaveOpen(perv => !perv)}/>
