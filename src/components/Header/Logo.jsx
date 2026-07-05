e838c1c65b052e5aea9d84771d8cf72c4e11a77e
mpcl5 <mpcl5mail@gmail.com>
2026-07-05 11:46:40 +0200

    header fixes

--- src/components/Header/Logo.jsx
+++ src/components/Header/Logo.jsx
@@ -1,8 +1,13 @@
-import styles from "./Logo.module.scss"
-import {Fira_Mono} from "next/font/google";
+import styles from "./Logo.module.scss";
+import { Fira_Mono } from "next/font/google";
 
-const mono = Fira_Mono({weight: "700", subsets: ['latin']});
+const mono = Fira_Mono({ weight: "700", subsets: ["latin"] });
 
 export const Logo = () => {
-    return <div className={`${styles.main} ${mono.className} text-primary`}>@MPCL5:~$</div>
-}
+  return (
+    <div className={`${styles.main} ${mono.className} text-primary`}>
+      @MPCL5:~$
+      <span className={styles.cursor} />
+    </div>
+  );
+};
